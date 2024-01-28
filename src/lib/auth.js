85728362c964e2f1992ba80c1ba8cc47e73c0c7c
user81 <ermachenya.al@yandex.by>
2024-01-28 22:22:12 +0300

    Next Auth with User Credentials Tutorial (Email & Password Login)

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,9 +1,11 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { connectToDb } from "./utils";
+import { User } from "./models";
 
 
 export const {
-  handlers: {GET, POST},
+  handlers: { GET, POST },
   auth,
   signIn,
   signOut,
@@ -14,4 +16,27 @@ export const {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({user, account, profile}) {
+      console.log(user, account, profile);
+      if (account.provider === "github") {
+        connectToDb();
+        try {
+          const user = await User.findOne({ email: profile.email });
+          if (!user) {
+            const newUser = new User({
+              username: profile.login,
+              email: profile.email,
+              image: profile.avatar_url,
+            });
+            await newUser.save();
+          }
+        } catch (error) {
+          console.log(err);
+          return false;
+        }
+      }
+      return true;
+    }
+  },
 });
