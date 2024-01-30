44dcde95c1be44be25b48e35a3cac7c4e21eb181
user81 <ermachenya.al@yandex.by>
2024-01-30 15:55:48 +0300

    Next Auth with User Credentials Tutorial (Email & Password Login)

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,4 +1,4 @@
-import { handleGithubLogin } from "@/lib/action";
+import { handleGithubLogin, login } from "@/lib/action";
 import styles from "./login.module.css";
 /* import { auth } from "@/lib/auth"; */
 
@@ -12,6 +12,11 @@ const LoginPage = async () => {
       <div className={styles.wrapper}>
         <form action={handleGithubLogin}>
           <button className={styles.github}>Login with Github</button>
+        </form>
+        <form action={login}>
+          <input type="text" placeholder="username" name="username" />
+          <input type="password" placeholder="password" name="password" />
+          <button>Login with Credentials</button>
         </form>
       </div>
     </div>
