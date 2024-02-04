2abd4a0eeff98bc79a9f94935793814ebbbdc282
user81 <ermachenya.al@yandex.by>
2024-02-04 18:50:22 +0300

    add error

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,9 +1,15 @@
-"use client"
+"use client";
+
+import style from "@/app/error.module.css";
 
 const Error = () => {
   return (
-    <div>Error</div>
-  )
-}
+    <div className={style.container}>
+      <div className={style.error}>
+        <h1>Error 404</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
